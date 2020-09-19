02b6b8a1f0e95242e4101ec7e7580bd857d61634
Lanea Lucy <lanealucy@gmail.com>
2020-09-19 18:24:27 +0200

    repaired plugin

--- plugins/yauw.php
+++ plugins/yauw.php
@@ -16,8 +16,12 @@ class yauw
 			// Do something with the Data
 			$received_message = $data->{'message'}->{'text'};
 			$fromID = $data->{'message'}->{'from'}->{'id'};
-			if ((preg_match('/^((.)* )?y.?a.?u.?w.?( (.)*)?$/i', $received_message)) == 1 && !$fromID = 25737932) {
-				$answer = "replay|DON'T SAY THIS AGAIN!!! You have not the Permission to do that!";
+			if ((preg_match('/^((.)* )?y.?a.?u.?w.?( (.)*)?$/i', $received_message)) == 1) {
+				if ($fromID == 25737932) {
+					$answer = "replay|yauw";
+				} else {
+					$answer = "replay|DON'T SAY THIS AGAIN!!! You have not the Permission to do that!";
+				}
 				return $answer;
 			}
 			break;
